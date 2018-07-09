e51d421fbb8d28767ecbeaec8d4dea4b26b116c9
Nathan Jones <njones@suse.com>
2018-07-09 10:25:36 +0100

    Added vue-rx

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import VueRx from 'vue-rx';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueRx)
 
 new Vue({
   router,
