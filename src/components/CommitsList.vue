f7afe75546d3d0233dc6fb7d4dadeea34c62cbea
Nathan Jones <njones@suse.com>
2018-07-11 15:29:34 +0100

    Add commit list WIP

---
+++ src/components/CommitsList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{commits}}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { getISO, getRepoKey } from '@/modules/helpers';
+import { Component } from 'vue-property-decorator';
+@Component
+export default class CommitsList extends Vue {
+  get repo() {
+    return this.$store.state.repoDetails.name;
+  }
+  get username() {
+    return this.$store.state.repoDetails.username;
+  }
+
+  get date() {
+    return getISO(this.$store.getters.getDate);
+  }
+  get commits() {
+    return this.$store.getters.getCommits(this.repo, this.username, this.date);
+  }
+}
+</script>
