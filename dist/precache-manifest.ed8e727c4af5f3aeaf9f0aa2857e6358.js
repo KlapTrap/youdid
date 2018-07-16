d1fbbc69e98a18034ed723251d4620c073331c0a
Nathan Jones <njones@suse.com>
2018-07-16 09:57:16 +0100

    Added dist for heroku

---
+++ dist/precache-manifest.ed8e727c4af5f3aeaf9f0aa2857e6358.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38586afaa9551dd2a232",
+    "url": "/js/chunk-vendors.38586afa.js"
+  },
+  {
+    "revision": "ba8cf6b295505dbfa24c",
+    "url": "/js/app.ba8cf6b2.js"
+  },
+  {
+    "revision": "28f9e2a075fc2fc19977b0a78141fa6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38586afaa9551dd2a232",
+    "url": "/css/chunk-vendors.a5b62414.css"
+  },
+  {
+    "revision": "ba8cf6b295505dbfa24c",
+    "url": "/css/app.65cc7ac8.css"
+  }
+];
